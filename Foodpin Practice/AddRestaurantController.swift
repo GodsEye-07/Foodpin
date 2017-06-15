1c02304e0a9468bddd537513e30fe03108836f57
Ayush Verma <ayush.verma2016@vitstudent.ac.in>
2017-06-15 05:37:06 +0530

    starting of Core Data

--- Foodpin Practice/AddRestaurantController.swift
+++ Foodpin Practice/AddRestaurantController.swift
@@ -11,11 +11,19 @@ import UIKit
 class AddRestaurantController: UITableViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
     @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var restaurantTextField: UITextField!
+    @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var yesBtn: UIButton!
+    @IBOutlet weak var noBtn: UIButton!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,7 +50,57 @@ class AddRestaurantController: UITableViewController,UIImagePickerControllerDele
             photoImageView.contentMode = .scaleAspectFill
             photoImageView.clipsToBounds = true
         }
-        dismiss(animated: true, completion: nil)
+            //updating the constraints programically as soon as the image is upladed
+        
+        let leadingConstraint = NSLayoutConstraint(item: photoImageView, attribute:
+        NSLayoutAttribute.leading, relatedBy: NSLayoutRelation.equal, toItem:
+        photoImageView.superview, attribute: NSLayoutAttribute.leading, multiplier: 1,constant: 0)
+    leadingConstraint.isActive = true
+    
+    let trailingConstraint = NSLayoutConstraint(item: photoImageView,attribute: NSLayoutAttribute.trailing, relatedBy: NSLayoutRelation.equal,toItem: photoImageView.superview, attribute: NSLayoutAttribute.trailing,multiplier: 1, constant: 0)
+    trailingConstraint.isActive = true
+    
+    let topConstraint = NSLayoutConstraint(item: photoImageView, attribute:
+        NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem:
+        photoImageView.superview, attribute: NSLayoutAttribute.top, multiplier: 1,constant: 0)
+    topConstraint.isActive = true
+    
+    let bottomConstraint = NSLayoutConstraint(item: photoImageView, attribute:
+        NSLayoutAttribute.bottom, relatedBy: NSLayoutRelation.equal, toItem:
+        photoImageView.superview, attribute: NSLayoutAttribute.bottom, multiplier: 1,constant: 0)
+    bottomConstraint.isActive = true
+    dismiss(animated: true, completion: nil)
+        
+}
+    
+    @IBAction func saveData(_ sender: Any) {
+        
+        print("\(restaurantTextField.text!)")
+        print("\(locationTextField.text!)")
+        print("\(typeTextField.text!)")
+        
+        if (restaurantTextField.text! == "") || (locationTextField.text! == "") || (typeTextField.text! == ""){
+            
+            let alertMessage = UIAlertController(title: "Oops !", message: "One of the fields has been left empty, We cannot proceed if any field is left Empty", preferredStyle: .alert)
+            let alert = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alertMessage.addAction(alert)
+            present(alertMessage, animated: true, completion: nil)
+        
+        }
+    }
+    
+    @IBAction func yesBtnPressed(_ sender: Any) {
+        
+        yesBtn.backgroundColor = UIColor.orange
+        noBtn.backgroundColor = UIColor.gray
+        
+    }
+    
+    @IBAction func noBtnPressed(_ sender: Any){
+        
+        yesBtn.backgroundColor = UIColor.gray
+        noBtn.backgroundColor = UIColor.orange
+    
     }
     
     
